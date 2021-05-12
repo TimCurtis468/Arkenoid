e256df01d8e4d7b532ba3c2fcbb3101ed2bbc620
TimCurtis468 <tim_r_curtis@hotmail.com>
2021-05-12 18:53:01 +0100

    Paddle movement
    Breakout/Arkanoid - Unity tutorial (E02: Paddle movement) complete and working
    
    https://www.youtube.com/watch?v=G4IuEdFqTWU

---
+++ Arkenoid/Assets/Scripts/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float paddleInitialY;
+    private float defaultPaddleWidthInPixels = 200;
+    private float defaultLeftClamp = 135;
+    private float defaultRightClamp = 410;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleInitialY = transform.position.y;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float paddleShift = (defaultPaddleWidthInPixels - ((defaultPaddleWidthInPixels / 2) * sr.size.x)) / 2;
+        float leftClamp = defaultLeftClamp - paddleShift;
+        float rightClamp = defaultRightClamp + paddleShift;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
+        transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
+    }
+
+}
