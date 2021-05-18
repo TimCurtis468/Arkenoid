1d732a842555092eae2ea16607584cf913e06015
TimCurtis468 <tim_r_curtis@hotmail.com>
2021-05-18 16:54:32 +0100

    Loaded in bricks for levels
    Loaded in bricks for levels in BricksManager

--- Arkenoid/Assets/Scripts/BricksManager.cs
+++ Arkenoid/Assets/Scripts/BricksManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,12 +22,52 @@ public class BricksManager : MonoBehaviour
     }
     #endregion
 
+    private int maxRows = 17;
+    private int maxCols = 12;
+
     [SerializeField]
     public Sprite[] Sprites;
 
+    public List<int[,]> LevelsData {get; set;}
+
     private void Start()
     {
-       
+        this.LevelsData = LoadLevelsData();   
+    }
+
+    private List<int[,]> LoadLevelsData()
+    {
+        TextAsset text = Resources.Load("levels") as TextAsset;
+
+        string[] rows = text.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<int[,]> levelsData = new List<int[,]>();
+        int[,] currentLevel = new int[maxRows, maxCols];
+        int currentRow = 0;
+        for(int row  = 0; row < rows.Length; row++)
+        {
+            string line = rows[row];
+
+            if(line.IndexOf("--") == -1)
+            {
+                string[] bricks = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for(int col = 0; col < bricks.Length; col++)
+                {
+                    currentLevel[currentRow, col] = int.Parse(bricks[col]);
+                }
+
+                currentRow++;
+            }
+            else
+            {
+                // End of level
+                currentRow = 0;
+                levelsData.Add(currentLevel);
+                currentLevel = new int[maxRows, maxCols];
+            }
+        }
+
+        return levelsData;
     }
 
     private void Update()
